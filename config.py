28d24462148a2a1dafd9bdcca63c2970541883f0
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2022-01-06 02:25:56 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+mail_username = 's.sonora@utopiansworld.com'
+mail_password = 'O[sga]ANI77INA'
+secret = '6LeWuNsdAAAAAFy8SvxdktnglXlPILhYvxao6Ygc'
+sitekey = '6LeWuNsdAAAAADcxyfSU5dlQEMzxI9Xm3KnQP6zh'
