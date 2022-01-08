ef42af1246cab83a19c2087133c586c2e65b89a6
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2022-01-08 13:24:01 -0300

    Update index.py

--- index.py
+++ index.py
@@ -20,9 +20,8 @@ def home():
         mensaje = request.form['mensaje']
         msg = Message(subject=f"Contact from UTOPIA", 
                       body=f"Nombre: {nombre}\nEmail: {email}\nMensaje: {mensaje}", 
-                      subject=HHHHAAAA, 
                       sender=(nombre, email),
-                      recipients=['santiagochinas@hotmail.com'])        
+                      recipients=['santiagochinas@hotmail.com', 'santiagosonora@gmail.com'])        
         mail.send(msg)
         return render_template('thanks.html', nombre=nombre)
     return render_template('index.html')
