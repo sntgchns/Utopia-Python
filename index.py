c2512f360961bbd7e3f0bd6308cd1ede34e12261
Santiago Soñora <86539574+sntgchns@users.noreply.github.com>
2022-01-07 23:07:38 -0300

    Update index.py

--- index.py
+++ index.py
@@ -24,7 +24,7 @@ def home():
                       sender=mail_username, 
                       recipients=['santiagochinas@hotmail.com'])        
         mail.send(msg)
-        return render_template('thanks.html')
+        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
